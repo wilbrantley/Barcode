5c74549fcbeed2788290bc7a8551d60b0dda9ab8
wilbrantley <101814698+wilbrantley@users.noreply.github.com>
2022-05-25 15:41:55 -0400

    Add files via upload

---
+++ IronBarcode/IronBarcode/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IronBarCode;
+
+namespace IronBarcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var barcode = BarcodeWriter.CreateBarcode("This is my first barcode by Iron Barcode", BarcodeEncoding.Code128);
+            barcode.AddBarcodeValueTextBelowBarcode();
+            barcode.SaveAsPng("Barcode.png");
+        }
+    }
+}
